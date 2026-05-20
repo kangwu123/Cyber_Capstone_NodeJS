deb19a5bcdfd0c8730138cc903692610a24c6938
Khang Vu <kangwu123@users.noreply.github.com>
2026-05-20 20:32:55 +0700

    Update minnor change

--- fiverr-system/src/common/constants/app.constants.ts
+++ fiverr-system/src/common/constants/app.constants.ts
@@ -1,15 +1,15 @@
 import 'dotenv/config';
 
-export const PORT = process.env.PORT;
-export const DATABASE_URL = process.env.DATABASE_URL;
+export const PORT = process.env.PORT || 3069;
+export const DATABASE_URL = process.env.DATABASE_URL || '';
 
 export const FOLDER_IMAGE = "public/images";
 
-export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
-export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
-export const ACCESS_TOKEN_EXPIRES_IN = process.env.ACCESS_TOKEN_EXPIRES_IN;
-export const REFRESH_TOKEN_EXPIRES_IN = process.env.REFRESH_TOKEN_EXPIRES_IN;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || '';
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || '';
+export const ACCESS_TOKEN_EXPIRES_IN = process.env.ACCESS_TOKEN_EXPIRES_IN || '';
+export const REFRESH_TOKEN_EXPIRES_IN = process.env.REFRESH_TOKEN_EXPIRES_IN || '';
 
-export const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME;
-export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
-export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+export const CLOUDINARY_NAME = process.env.CLOUDINARY_NAME || '';
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || ''; 
+export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || '';
